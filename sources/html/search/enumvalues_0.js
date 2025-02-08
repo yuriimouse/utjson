1a2a32c51f41d628fee0f958326bf55cb0d85f2e
Iurii Prudius <iprudius@geeksforless.com>
2025-02-08 15:00:50 +0200

    documentation

--- sources/html/search/enumvalues_0.js
+++ sources/html/search/enumvalues_0.js
@@ -5,5 +5,6 @@ var searchData=
   ['utjson_5fnull',['utjson_NULL',['../group__types.html#ga1ca98d67f62e7feb99069da91df062b0aea65da113444e4dd959de19b63028672',1,'utjson_NULL():&#160;utjson.dox.h'],['../group__types.html#ga1ca98d67f62e7feb99069da91df062b0aea65da113444e4dd959de19b63028672',1,'utjson_NULL():&#160;utjson.h']]],
   ['utjson_5fnumber',['utjson_NUMBER',['../group__types.html#ga1ca98d67f62e7feb99069da91df062b0a83d0661a184610e58fa524113bc34d2c',1,'utjson_NUMBER():&#160;utjson.dox.h'],['../group__types.html#ga1ca98d67f62e7feb99069da91df062b0a83d0661a184610e58fa524113bc34d2c',1,'utjson_NUMBER():&#160;utjson.h']]],
   ['utjson_5fobject',['utjson_OBJECT',['../group__types.html#ga1ca98d67f62e7feb99069da91df062b0a34837d65bab94365e9bb7662b434f80c',1,'utjson_OBJECT():&#160;utjson.dox.h'],['../group__types.html#ga1ca98d67f62e7feb99069da91df062b0a34837d65bab94365e9bb7662b434f80c',1,'utjson_OBJECT():&#160;utjson.h']]],
+  ['utjson_5fpointer',['utjson_POINTER',['../group__types.html#ga1ca98d67f62e7feb99069da91df062b0a86fe727c05dcf62e89a2390964fca8a8',1,'utjson.h']]],
   ['utjson_5fstring',['utjson_STRING',['../group__types.html#ga1ca98d67f62e7feb99069da91df062b0ab3ad17c4a26a717c2b988fd457afa623',1,'utjson_STRING():&#160;utjson.dox.h'],['../group__types.html#ga1ca98d67f62e7feb99069da91df062b0ab3ad17c4a26a717c2b988fd457afa623',1,'utjson_STRING():&#160;utjson.h']]]
 ];
--- sources/html/search/variables_4.js
+++ sources/html/search/variables_4.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['parent',['parent',['../structutjson.html#ad1a76068b2682ca464faaa9b12964b21',1,'utjson']]]
+  ['parent',['parent',['../structutjson.html#ad1a76068b2682ca464faaa9b12964b21',1,'utjson']]],
+  ['pointer',['pointer',['../structutjson.html#a2f9e2565eaea27cec92f6fe817155ca9',1,'utjson']]],
+  ['pointer_5ftype',['pointer_type',['../structutjson.html#a20c9241b1ebb4d4aa4a9e62d4ff444fb',1,'utjson']]]
 ];
